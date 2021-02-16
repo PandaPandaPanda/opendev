9086bfc6878d34c7687c1ead199b9a173d8ddc31
PandaPandaPanda <34975856+PandaPandaPanda@users.noreply.github.com>
2021-02-16 10:00:10 -0500

    Updates

---
+++ precache-manifest.ec6139774efa5069c7ae68ba93a2d65c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3527d35672c9f575bbbb7f89f11ef6",
+    "url": "/opendev/index.html"
+  },
+  {
+    "revision": "d47b71bf37832d004c1c",
+    "url": "/opendev/static/css/main.f229b4f6.chunk.css"
+  },
+  {
+    "revision": "5236e7c7facd36c57769",
+    "url": "/opendev/static/js/2.c28b0e47.chunk.js"
+  },
+  {
+    "revision": "d47b71bf37832d004c1c",
+    "url": "/opendev/static/js/main.8e9330f1.chunk.js"
+  },
+  {
+    "revision": "3784e3b6442ec79950e1",
+    "url": "/opendev/static/js/runtime~main.dfe117d0.js"
+  }
+]);
